bda16ad19792e01a5a59f300936d77e6760d155a
AkmalAlif <38677551+AkmalAlif@users.noreply.github.com>
2018-07-25 09:43:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+
+<style>
+
+.topnav a{
+	float: right;
+	display: block;
+	background-color: white;
+	color: lightblue;
+	text-align: center;
+	padding: 20px 22px;
+	text-decoration: none;
+	}
+	
+.topnav a:hover{
+	background-color: white;
+	color: black;
+	}
+
+
+body{
+	background-image:url('gambar1.jpg');
+	background-size:cover;
+	font-family:sans-serif;
+	font-color:white;
+	}
+
+</style>
+
+<body>
+<center>
+<marquee><h1>Selamat Datang ke Taman Astakona</h1></marquee>
+</center>
+<p>
+
+<div class="topnav a">
+	<a href="senarai.php">Senarai penduduk</a>
+	<a href="form.php">Daftar masuk</a>
+	<a href="index.php">Utama</a>
+</div>
+
+
+</html>
